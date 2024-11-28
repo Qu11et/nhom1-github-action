e224e857fa7aa3f88981288be5d4cc7478b1383a
TaiKhau <lekhauhuutai48@gmail.com>
2024-11-28 13:35:33 +0700

    update from local

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "mario123bucket" # Replace with your actual S3 bucket name
+    bucket = "group1awsbucket" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
-    region = "ap-south-1"
+    region = "us-east-1"
   }
 }
